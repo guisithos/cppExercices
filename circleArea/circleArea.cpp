59a2a2d0ef273edb524fc15313750b6e7f174306
Guilherme Thomas <gui.thomas@outlook.com>
2022-04-24 23:25:06 -0300

    Update circle

--- circleArea/circleArea.cpp
+++ circleArea/circleArea.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int a;
+    void circleArea(int);  //Function
+    cout<<"\nDigite o raio: ";
+    cin>>a;
+    circleArea(a);  //Call
 
+
+}
+void circleArea(int r)
+{
+    cout<<"\nArea do circulo eh: "<<3.14*r*r;
+}
